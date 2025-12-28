b12f248e5b36488ef9fb6fe0b035758c2b20331b
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2025-12-28 22:10:20 +0100

    Diagrammes

---
+++ DB.sql
@@ -0,0 +1,38 @@
+use unitycareclinic;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+
+
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(100)
+);
+
+
+CREATE TABLE doctors (
+    doctor_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    specialty VARCHAR(100),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    email VARCHAR(150) UNIQUE,
+    phone VARCHAR(30),
+);
+CREATE TABLE patients (
+    patient_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    gender ENUM('Male', 'Female') NOT NULL,
+    birth_date DATE,
+    phone VARCHAR(30),
+    email VARCHAR(150),
+    address VARCHAR(150)
+);
